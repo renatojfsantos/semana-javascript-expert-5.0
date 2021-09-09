2e80edaef7aa0cc5a34990a38183cee570054a1f
Renato Santos <renato_jfs@hotmail.com>
2021-09-08 23:58:34 -0300

    Segunda aula

---
+++ aulas/aula02/gdrive-webapi/test/_util/testUtil.js
@@ -0,0 +1,60 @@
+import { jest } from '@jest/globals'
+import { Readable, Writable, Transform } from 'stream'
+export default class TestUtil {
+
+  static mockDateNow(mockImplementationPeriods) {
+
+    const now = jest.spyOn(global.Date, global.Date.now.name)
+
+    mockImplementationPeriods.forEach(time => {
+      now.mockReturnValueOnce(time)
+    })
+
+  }
+
+  static getTimeFromDate(dateString) {
+    return new Date(dateString).getTime()
+  }
+
+  static generateReadableStream(data) {
+    return new Readable({
+      objectMode: true,
+      read() {
+        for (const item of data) {
+          this.push(item)
+        }
+
+        this.push(null)
+      }
+    })
+  }
+
+  static generateWritableStream(onData) {
+    return new Writable({
+      objectMode: true,
+      write(chunk, encondig, cb) {
+        onData(chunk)
+
+        cb(null, chunk)
+      }
+    })
+  }
+
+  static generateTransformStream(onData) {
+    // async function *(source) {
+    //   for await(const chunck of data) {
+    //     yield chunck
+    //   }
+    // }
+
+    return new Transform({
+      objectMode: true,
+      transform(chunk, encondig, cb) {
+        onData(chunk)
+
+        cb(null, chunk)
+      }
+    })
+  }
+
+}
